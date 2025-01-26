ff3352492ff4e76d6e8d8089d7f7b7586776f9c2
soumyayalahankaajjegowdagit config --global user.name soumyayalahankaajjegowda <soumya.ajjegowda@gmail.com>
2025-01-26 15:15:05 -0700

    lastprogress

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
-const express = require('express')
-const app = express()
-
-//routes
-
-app.get('/',(req,res) => {
-    res.send('Hello Node API')
-})
-
-app.listen(3000, ()=> {
-    console.log(`Node API app is running on port 3000`)
-})
+//
+const express = require ('express');
